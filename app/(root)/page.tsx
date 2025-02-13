2ed58c04166bc273b48d8a04b62f211683795667
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-02-13 18:38:41 -0300

    feat: adicionar ShadCN UI, estilizar globals.css e criar Hero Section.
    - Integrada a biblioteca de componentes UI ShadCN ao projeto.
    - Finalizada a estilização do globals.css para padronização visual.
    - Criada a estrutura e estilização da Hero Section.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">home</h1>
+      <section className="blue_container">
+        <h1 className="heading">home</h1>
+        <p className="sub-heading !max-w-3xl">lorem test and show</p>
+      </section>
     </>
   );
 }
