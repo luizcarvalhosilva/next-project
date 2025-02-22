8df685f0a26ded0c045a49003832bd33e3ed5d9e
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-02-22 14:51:07 -0300

    feat: importar estilo do Markdown Editor e ajustes visuais.
    - Importado `easymde.min.css` no `layout.tsx` para estilização do editor Markdown do Sanity.
    - Ajustadas cores para seguir o padrão correto.
    - Removido comentário desnecessário.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
 			},
 			colors: {
 				primary: {
-					"100": "#FFE8F0",
+					"100": "#D6F2FE",
 					DEFAULT: "#04A2EB",
 				},
 				secondary: "#F17300",
@@ -41,7 +41,7 @@ const config: Config = {
 			boxShadow: {
 				100: "2px 2px 0px 0px rgb(0, 0, 0)",
 				200: "2px 2px 0px 2px rgb(0, 0, 0)",
-				300: "2px 2px 0px 2px rgb(238, 43, 105)",
+				300: "2px 2px 0px 2px rgb(241, 116, 0)",
 			},
 		},
 	},
