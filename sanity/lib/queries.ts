44b1b85dfb25b7c166488d5c57b70244b117b5d7
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-03-24 18:04:26 -0300

    feat: implementação do fluxo de autenticação do autor
    - Adicionado suporte à autenticação de autores.
    - Ajustes nas permissões para garantir que apenas usuários autenticados possam gerenciar suas startups.

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -30,3 +30,15 @@ export const STARTUP_BY_ID_QUERY =
   image,
   pitch,
 }`);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+  *[_type == "author" && id == $id][0]{
+  _id,
+  id,
+  name,
+  username,
+  email,
+  image,
+  bio
+}
+`);
