b3812e2be18b3f168248c095ef14c0d3e9b24900
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2025-04-17 10:19:01 -0300

    feat(user-profile): cria a página de perfil do usuário e seus estilos
    Implementa a estrutura da tela de perfil do usuário (UserProfile) com os estilos correspondentes, preparando o layout para receber dados dinâmicos futuramente.

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 import Image from "next/image";
 import { EyeIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import { formatDate } from "../lib/utils";
+import { cn, formatDate } from "../lib/utils";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
+
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -37,7 +39,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                 </div>
 
                 <Link href={`/user/${author?._id}`}>
-                    <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
+                    <Image src={author?.image!} alt="placeholder" width={48} height={48} className="rounded-full" />
                 </Link>
             </div>
 
@@ -57,5 +59,15 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </li >
     );
 };
+
+export const StartupCardSkeleton = () => (
+    <>
+        {[0, 1, 2, 3, 4].map((index: number) => (
+            <li key={cn("skeleton", index)}>
+                <Skeleton className="startup-card_skeleton" />
+            </li>
+        ))}
+    </>
+)
 
 export default StartupCard;
